eac263b3e18c26e2092f36d54bb6e85b4eb43fc2
ChrisWalkerDev <chris.walker.automations@gmail.com>
2024-06-17 22:48:31 -0400

    Updated jenkins file

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 import express from "express";
 
 const app = express();
-const env = process.env.NODE_ENV;
+const env = process.env.NODE_ENV || 'LOCAL';
 const PORT = process.env.PORT || 8080;
+const version = process.env.VERSION || 'LOCAL';
+
+app.get('/version', (req, res) => {
+    res.json({'version': version});
+})
 
 app.use(express.static('src'))
 
